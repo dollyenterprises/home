c4c879edf08591a708f12aa7ad59e6e8d4a6fa98
dollyenterprises <104253545+dollyenterprises@users.noreply.github.com>
2023-08-03 20:49:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# home
+# dollyenterprises.in
---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+    if(isset($_POST['Name'])){
+        $name = $_POST['Name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        
+    }
+?>
